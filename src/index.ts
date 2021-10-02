70c1e181104fb04c4e39dc60b8c0b5f14f80c324
Simon Olander <simon.olander@r2m.se>
2021-10-02 18:23:56 +0200

    Form complete

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
+const usernameInput = document.getElementById("username") as HTMLInputElement
+const passwordInput = document.getElementById("password") as HTMLInputElement
+const passwordHint = document.getElementById("password-hint") as HTMLParagraphElement
 
+async function setUsername() {
+    const username = "Alfons"
+    usernameInput.value = username
+    return username
+}
+
+function onPasswordChange() {
+    passwordInput.classList.remove("is-danger")
+}
+
+function onFormSubmit(): false {
+    passwordInput.classList.add("is-danger")
+    return false
+}
+
+async function main() {
+    const username = await setUsername()
+}
+
+main().catch(console.error)
