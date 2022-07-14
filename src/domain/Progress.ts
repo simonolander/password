9f6f197fbdd36efe6ff49ffba7b5b175c1e8f151
Simon Olander <simon.olander@r2m.se>
2022-07-14 20:23:21 +0200

    Decoders

--- src/domain/Progress.ts
+++ src/domain/Progress.ts
@@ -1,3 +1,5 @@
+import {array, constant, Decoder, object, string, taggedUnion} from "decoders";
+
 export interface CompletedLevel {
     levelId: string
     solution: string
@@ -7,32 +9,54 @@ export interface Progress {
     completedLevels: CompletedLevel[]
 }
 
-interface CompletedLevelDtoV1 {
-    version: "1"
-    levelId: string
-    solution: string
+interface CompletedLevelDto {
+    toDomain: () => CompletedLevel
 }
+
+class CompletedLevelDtoV1 implements CompletedLevelDto {
+    constructor(private levelId: string, private solution: string) {
+    }
+
+    toDomain(): CompletedLevel {
+        return {
+            levelId: this.levelId,
+            solution: this.solution,
+        }
+    }
+}
+
+const completedLevelV1Decoder: Decoder<CompletedLevel> = object({
+    version: constant("1"),
+    levelId: string,
+    solution: string,
+}).transform(it => new CompletedLevelDtoV1(it.levelId, it.solution))
 
 interface ProgressDtoV1 {
     version: "1"
     completedLevels: CompletedLevelDtoV1[]
 }
 
-export function loadProgress(): Progress | undefined {
+const progressDtoV1Decoder: Decoder<Progress> = object({
+    version: constant("1"),
+    completedLevels: array(completedLevelDtoV1Decoder)
+})
+
+const progressDecoder: Decoder<Progress> = taggedUnion("version", {"1": progressDtoV1Decoder})
+
+export function loadProgress(): Progress | null {
     try {
-        const json = localStorage.getItem("progress");
+        const json = localStorage.getItem("progress")
         if (json === null) {
-            return undefined
+            return null
         }
-        let object = JSON.parse(json);
+        let object = JSON.parse(json)
         if (object.hasOwnProperty("version")) {
             if (object.version === "1") {
                 return
             }
         }
-    }
-    catch (e) {
+    } catch (e) {
         console.warn("Could not parse progress in local storage", e)
-        return undefined
+        return null
     }
 }
